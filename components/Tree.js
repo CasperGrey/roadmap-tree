b06feeb3a6d978503f8992a405c95b1c3b06a83f
CasperGrey <mrcaspergrey@gmail.com>
2024-12-27 15:10:45 +1100

    Create Tree.js

---
+++ components/Tree.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+import Node from './Node';
+import Phase from './Phase';
+import config from '../utils/config';
+import './Tree.css';
+
+const Tree = ({ roadmapData }) => {
+  return (
+    <div className="tree">
+      <h1 className="tree-title">The Tree</h1>
+      <div className="phases">
+        {roadmapData.map((phase, index) => (
+          <Phase
+            key={index}
+            name={phase.phase}
+            focus={phase.focus}
+            years={phase.years}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tree;
